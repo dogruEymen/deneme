ed30e4e0479d2ff39c68c196579b2ff295dadfa1
Eymen Doğru <muh.eymendogru@gmail.com>
2026-04-07 18:42:51 -0600

    some changes

--- build.groovy
+++ build.groovy
@@ -1,12 +1,11 @@
-properties([
-    pipelineTriggers([
-        pollSCM('* * * * *') // Her dakika (Dakika Saat Gün Ay HaftanınGünü)
-    ])
-])
-
 node {
-    stage('Kontrol') {
-        checkout scm // Bu satır kritik! (Açıklaması aşağıda)
-        echo "Kontrol edildi: " + sh(script: 'date', returnStdout: true)
+    stage("Selamla"){
+        
+        println("Hello World")
+
+        echo "hello"
+
+        sh 'date'
     }
+    
 }
